f2844f8c34d41c597b16c7363c81fa149051a624
Heewon Cho <heewon6752@gmail.com>
2024-01-29 17:20:10 +0900

    Add common

---
+++ platform/common/include/common/x86/serial.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: MPL-2.0 */
+/* Copyright (c) 2024, Pribess (Heewon Cho).
+ * Licensed under the MPL-2.0 License.
+ * You may not use this file except in compliance with the License.
+ */
+
+#ifndef __SERIAL_H__
+#define __SERIAL_H__
+
+void serial_init();
+
+#endif /* __SERIAL_H__ */
---
+++ platform/common/x86/serial.c
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: MPL-2.0 */
+/* Copyright (c) 2024, Pribess (Heewon Cho).
+ * Licensed under the MPL-2.0 License.
+ * You may not use this file except in compliance with the License.
+ */
+
+#include <common/x86/serial.h>
+
+void serial_init() {
+    #define PORT 0x3f8          // COM1
+
+	outb(PORT + 1, 0x00);
+	outb(PORT + 3, 0x80);
+	outb(PORT + 0, 0x03);
+	outb(PORT + 1, 0x00);
+	outb(PORT + 3, 0x03);
+}
